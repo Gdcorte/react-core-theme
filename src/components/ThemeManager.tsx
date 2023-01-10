32c6c65ff9290188f598b9e6e81ea30ff8dc8a38
Gustavo Diniz da Corte <gustavodinizdacorte@Gustavos-MacBook-Pro.local>
2023-01-10 09:43:19 +0900

    added default fallback for base theme

--- src/components/ThemeManager.tsx
+++ src/components/ThemeManager.tsx
@@ -18,16 +18,20 @@ const ThemeManager: FunctionComponent<ThemeManagerProps> = ({
     return bundleBaseThemes();
   }, []);
 
+  // Default fallback in case of invalid combination of type/color
+  const type = themes[typeName] ? typeName : "dark";
+  const color = themes[type].colors[colorName] ? colorName : "Green";
+
   return (
     <ThemeProvider
       theme={{
-        presets: themes[typeName].presets,
-        fonts: themes[typeName].fonts,
-        background: themes[typeName].background,
-        alerts: themes[typeName].alerts,
-        colors: themes[typeName].colors[colorName],
-        typeName,
-        colorName,
+        presets: themes[type].presets,
+        fonts: themes[type].fonts,
+        background: themes[type].background,
+        alerts: themes[type].alerts,
+        colors: themes[type].colors[color],
+        typeName: type,
+        colorName: color,
       }}
     >
       {children}
